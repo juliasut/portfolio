7e6c24d997fc4884c48cf187cbd1ffa94d68c032
juliasut <juliasutygina@gmail.com>
2023-01-18 02:01:08 -0700

    use HashRouter

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -4,7 +4,7 @@ import Navbar from './Navbar';
 import Home from './home/Home';
 import About from './about/About';
 import Portfolio from './portfolio/Portfolio';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, HashRouter } from 'react-router-dom';
 import { Box, Grid } from '@mui/material';
 
 export default function BaseLayout() {
@@ -40,13 +40,15 @@ export default function BaseLayout() {
         <Grid item>
           <Navbar darkMode={darkMode} handleClick={handleToggleDarkMode} />
         </Grid>
-        <Grid item flexGrow={1}>
-          <Routes>
-            <Route exact path={'/'} element={<Home />} />
-            <Route exact path={'/about'} element={<About />} />
-            <Route exact path={'/portfolio'} element={<Portfolio />} />
-          </Routes>
-        </Grid>
+        <HashRouter>
+          <Grid item flexGrow={1}>
+            <Routes>
+              <Route exact path={'/'} element={<Home />} />
+              <Route exact path={'/about'} element={<About />} />
+              <Route exact path={'/portfolio'} element={<Portfolio />} />
+            </Routes>
+          </Grid>
+        </HashRouter>
         <Grid item>
           <Box
             component={'footer'}
